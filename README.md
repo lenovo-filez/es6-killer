ce94e1ffb9d08a8a32fb74ceb1d3d40c03fbba80
蔺超 <linchao@lenovocloud.com>
2020-11-25 17:23:01 +0800

    feat: test1,测试commit钩子能否识别忽略文件

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # es6-killer
 use es-check to check ecmascript2015 for project, don't allow es6 to destory your project
+
+# 全局扫描
+npm run es-check
+
+# commit扫描
+use 'git commit' ,husky will tell you the files with problems
---
+++ index.js
@@ -0,0 +1,30 @@
+const { exec } = require('child_process')
+const path = require('path')
+// 忽略的黑名单
+// 黑名单写法：不可使用'./'写法，允许'/'或者'文件夹名开头'
+const blackList = [
+  '/index.js',
+  '/node_modules',
+  '/build',
+  '/www/mobile',
+  '/www/resource/pdfPlugin',
+  '/www/js/gallery',
+  '/www/js/module/Upload',
+  '/www/js/link_share/link_mobile',
+  '/www/js/link_approval/link_mobile'
+]
+const ignoreNames = blackList.join(',')
+const checkFilesArr = process.argv.slice(2).map(item => {
+  return path.resolve(__dirname , item)
+})
+const checkFiles = checkFilesArr.join(' ') || './**/*.js'
+exec(`npx es-check es5 --not="${ignoreNames}" ${checkFiles}`, function (err, stdout, stderr) {
+  if(err) {
+    console.log(stderr)
+    console.log(stdout)
+    process.exit(err.code)
+  } else {
+    console.log(stderr)
+    process.exit(0)
+  }
+})
